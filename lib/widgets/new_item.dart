6acee720e837e1082e3a5992b0c8b2f9050714b0
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-10-28 19:17:02 +0530

    removing unnecessary http call

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:http/http.dart' as http;
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -36,14 +37,20 @@ class _NewItemState extends State<NewItem> {
         }),
       );
 
-      print('status code: ${response.statusCode}');
-      print(' body: ${response.body}');
-      
+      final Map<String, dynamic> resData = json.decode(response.body);
+
       if (!context.mounted) {
         return;
       }
 
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
