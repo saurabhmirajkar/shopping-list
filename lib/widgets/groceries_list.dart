70138168b251be04e1cca2c7bd20c674493761bc
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-10-29 12:10:31 +0530

    handling response error 2.0

--- lib/widgets/groceries_list.dart
+++ lib/widgets/groceries_list.dart
@@ -29,37 +29,50 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https('shopping-list-35884-default-rtdb.firebaseio.com',
         'shopping-list.json');
 
-    final response =
-        await http.get(url, headers: {'Content-Type': 'authorization/json'});
+    try {
+      final response =
+          await http.get(url, headers: {'Content-Type': 'authorization/json'});
 
-    if (response.statusCode > 400) {
+      if (response.statusCode > 400) {
+        setState(() {
+          _error = 'Failed to fetch data. Please try again later.';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere((ctg) => item.value['category'] == ctg.value.title)
+            .value;
+
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+
       setState(() {
-        _error = 'Failed to fetch data. Please try again later.';
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong. Please try again later.';
       });
     }
-
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedItems = [];
-
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere((ctg) => item.value['category'] == ctg.value.title)
-          .value;
-
-      loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ),
-      );
-    }
-
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
   }
 
   void _addItem() async {
@@ -76,10 +89,26 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https('shopping-list-35884-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+
+    final response = await http.delete(url);
+
+    if (response.statusCode > 400) {
+      setState(() {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+                'Failed to delete ${item.name}. Please try again later.')));
+
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
